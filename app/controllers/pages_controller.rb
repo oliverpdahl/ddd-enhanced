988e3fd6db94c0ae5c9ab633c3aa7846e20d02a7
oliverpdahl <oliverpdahl@gmail.com>
2020-12-04 17:58:49 -0500

    added all to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def zipCodeAccidentGrowth
   end
+  def accidentTimeOfDayTrends
+  end
+  def zipBarChartRace
+  end
+  def outsideTempSeverityTrend
+  end
+  def lightsVsRoundAboutsYears
+  end
 end
