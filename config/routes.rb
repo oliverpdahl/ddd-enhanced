a3cb7fffed8c0cd194c90d107ca45962f7c369f8
oliverpdahl <oliverpdahl@gmail.com>
2020-12-04 18:00:50 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/zipCodeAccidentGrowth'
+  get 'pages/accidentTimeOfDayTrends'
+  get 'pages/zipBarChartRace'
+  get 'pages/outsideTempSeverityTrend'
+  get 'pages/lightsVsRoundAboutsYears'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
