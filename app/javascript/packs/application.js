c4646f7bce16c98d777456fe89b1047b947e0f28
oliverpdahl <oliverpdahl@gmail.com>
2020-12-05 15:35:15 -0500

    getting chart to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require highcharts
+//= require chartkick
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
@@ -12,7 +13,7 @@ import "bootstrap";
 import "../stylesheets/application";
 
 require("chartkick");
-require("chart.js");
+require("chartkick").use(require("highcharts"));
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
