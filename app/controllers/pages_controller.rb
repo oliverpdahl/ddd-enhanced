68c2e2c026072edcf7b50c6e07f59499cb53801a
Stephen Smith <autrydev@gmail.com>
2020-12-05 16:57:37 -0400

    Add SQL to Line Chart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,15 @@ class PagesController < ApplicationController
   def outsideTempSeverityTrend
   end
   def lightsVsRoundAboutsYears
+
+    sql = 'SELECT SUM("total count") "Total Tuples"
+        FROM((SELECT COUNT(*)"total count"
+            FROM area
+            UNION
+            SELECT COUNT(*) "total count"
+            FROM accident_all))'
+    
+    @result = ActiveRecord::Base.connection.exec_query(sql).to_a
+
   end
 end
