2f1f850b3a049ea1c8e3ed22549d0c184804b3aa
oliverpdahl <oliverpdahl@gmail.com>
2020-12-07 10:19:03 -0500

    doesn't break when empty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,25 +32,38 @@ class PagesController < ApplicationController
   def outsideTempSeverityTrend
 
     @inputs = params[:p]
-    @low = -100
+    @low = 0
     @high = 100
+    @min = 0
+    @min = 0
 
-    if (@inputs.blank?)
-      result = helpers.getAccidentSeverity()
+    if (@inputs.present?)
+      if(@inputs.values.present?)
+        if(@inputs.values[0].present?)
+          @low = @inputs.values[0]
+        end
+        if(@inputs.values[1].present?)
+          @high = @inputs.values[1]
+        end
+          result = helpers.getAccidentSeverityLowTemp(@low, @high)
+      else
+        result = helpers.getAccidentSeverity()
+      end
     else
-      @low = @inputs.values[0]
-      result = helpers.getAccidentSeverityLowTemp(0, 100)
+      result = helpers.getAccidentSeverity()
     end
 
     @acc_severity = result.collect{|i| [i['Mo-Year'],i['Average Severity']]}
 
-    @min = @acc_severity[0][1]
-    @max = @acc_severity[0][1]
-    for i in 0..(@acc_severity.length()-1)
-      if @acc_severity[i][1] < @min 
-        @min = @acc_severity[i][1]
-      elsif @acc_severity[i][1] > @max 
-        @max = @acc_severity[i][1]
+    if !@acc_severity.blank?
+      @min = @acc_severity[0][1]
+      @max = @acc_severity[0][1]
+      for i in 0..(@acc_severity.length()-1)
+        if @acc_severity[i][1] < @min 
+          @min = @acc_severity[i][1]
+        elsif @acc_severity[i][1] > @max 
+          @max = @acc_severity[i][1]
+        end
       end
     end
   end
